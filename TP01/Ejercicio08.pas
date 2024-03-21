24a0d0242da39f3d54a9244d4bb1086ec21e7906
constanza.giacoboni <constanza.giacoboni@whitebox.com.ar>
2024-03-21 18:32:19 -0300

    avance ejercicio 8

---
+++ TP01/Ejercicio08.pas
@@ -0,0 +1,22 @@
+Program Ejercicio08;
+
+Var x, y: integer;
+
+Procedure intercambio(var x,i: integer);
+var aux: integer;
+
+begin
+    aux := x;
+    x := y;
+    y := aux;
+end;
+
+begin
+    Write('Ingrese un valor');
+    read(x);
+    write('Ingrese otro valor');
+    read(y);
+    writeln('El valor de x es ',x,' y el valor de y es ',y);
+    intercambio(x,y);
+    writeln('El valor de x es ',x,' y el valor de y es ',y);
+end.
