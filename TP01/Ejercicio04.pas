27dc3ffb845b7215f2e479c24daea72b0e227afc
constanza.giacoboni <constanza.giacoboni@whitebox.com.ar>
2024-03-21 17:22:50 -0300

    avance ejercicio 4

---
+++ TP01/Ejercicio04.pas
@@ -0,0 +1,20 @@
+Program Ejercicio04;
+
+var num1: real;
+
+Procedure mayor(n1: real);
+
+Begin
+    if(n1>0) then
+        Write('Es mayor a 0.')
+    else if(n1<0) then
+        Write('Es menor a 0.')
+    else
+        Write('Es igual a 0.')
+End;
+
+Begin
+    Write('Ingrese un numero.');
+    Read(num1);
+    mayor(num1);
+End.
