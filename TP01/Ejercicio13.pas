071169ba76a0e6d6179b61a95bac66035adce317
constanza.giacoboni <constanza.giacoboni@whitebox.com.ar>
2024-03-31 00:13:25 -0300

    avance ejercicio 13

---
+++ TP01/Ejercicio13.pas
@@ -0,0 +1,18 @@
+Program Ejercicio13;
+
+procedure farenheit_to_celcius;
+
+var f: integer; res: real;
+
+begin
+    f := 0;
+    repeat
+        res := 5/9 * (f-32);
+        WriteLn ('Farenheit: ',f,' --> Celcius: ',res);
+        f := f + 10;
+    until f = 200;
+end;
+
+Begin
+    farenheit_to_celcius;
+End.
